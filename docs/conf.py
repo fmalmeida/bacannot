26a2572db54a5cb3f17a2d4716cd3f35eff4d8d0
Felipe Marques de Almeida <felipemarques89@gmail.com>
2021-04-09 15:07:07 -0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,12 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
