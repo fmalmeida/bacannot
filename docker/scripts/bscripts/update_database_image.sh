60491476210868b683b20aa8780df751176cacd7
fmalmeida <felipemarques89@gmail.com>
2021-05-06 15:15:35 -0300

    Update update_database_image.sh

--- docker/scripts/bscripts/update_database_image.sh
+++ docker/scripts/bscripts/update_database_image.sh
@@ -24,13 +24,28 @@ read -p "Do you really want to build it locally? (y/N) " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    echo "Starting docker local build!"
+    echo
+    echo "# Starting docker local build!"
     wget --quiet -O my_dockerfile https://github.com/fmalmeida/bacannot/raw/master/docker/Dockerfile_bacannot
-    docker build -t fmalmeida/bacannot:latest -f my_dockerfile .
-    docker rmi -f fmalmeida/bacannot_main_tools
+    #docker build -t fmalmeida/bacannot:latest -f my_dockerfile .
+    rm my_dockerfile
     echo
     echo "Image built!"
     echo
+
+    echo "# Environment cleaning"
+    echo
+    echo "Do you want to remove the base image fmalmeida/bacannot_main_tools?"
+    echo "When updating the databases again, if available, docker will automatically"
+    echo "skips the base image download."
+    read -p "Your answer? (y/N) " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      docker rmi -f fmalmeida/bacannot_main_tools
+    else
+      echo
+    fi
 else
     echo "You've chosen not to build! Giving up"
     echo
