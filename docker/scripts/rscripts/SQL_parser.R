a538681886dda3dbcd0d0a4bed6036f42f18822b
fmalmeida <felipemarques89@gmail.com>
2021-06-03 18:55:38 -0300

    Update SQL_parser.R
    fixing digIS in server

--- docker/scripts/rscripts/SQL_parser.R
+++ docker/scripts/rscripts/SQL_parser.R
@@ -1,26 +1,26 @@
 # Shiny GFF/SQLdb parser -- To understand the results of Bacannot
 parse_sql <- function(sqldb) {
-  
+
   shinyApp(
     options=list(height = '1500px', width = '100%'),
     ui = fluidPage(
-      
+
       # Custom CSS
       tags$head(
         tags$style(HTML("
-      body { 
+      body {
         /* Normal  */
         font-size: 12pt;
         line-height: 1.5;
       }
-        
+
       blockquote {
           padding: 11px 20px;
           margin: 0 0 20px;
           font-size: 10pt;
           border-left: 5px solid #ec5800;
       }
-        
+
       .table {
         width: 100% !important;
         text-align: center;
@@ -30,10 +30,10 @@ parse_sql <- function(sqldb) {
       }
 
     "))),
-      
+
       # Page theme
       theme = shinytheme("cosmo"),
-      
+
       # Page UIs
       mainPanel(
         h3("Main filters:"),
@@ -44,22 +44,22 @@ parse_sql <- function(sqldb) {
           column(2, textInput("start", "Min. start", value = "0")),
           column(2, textInput("end", "Max. end")),
           column(2, selectInput("strand", "Strand:",
-                                sort(c("+", "-")), 
+                                sort(c("+", "-")),
                                 multiple = TRUE))
         ),
         h3("Attributes filters:"),
-        markdown("> **Obs**: Using a file of patterns users can filter the annotation file based on 
-                 the values found in the attributes column. The file must have one pattern per line, 
+        markdown("> **Obs**: Using a file of patterns users can filter the annotation file based on
+                 the values found in the attributes column. The file must have one pattern per line,
                  for any of the fields available in the 9th column. Users can download the annotation
-                 in excel spreadsheet format to better visualize the values found in the attributes. 
+                 in excel spreadsheet format to better visualize the values found in the attributes.
                  The download button is found in the end of the page."),
         fileInput("att_patterns", "Input file of patterns (values)", multiple = FALSE),
         verbatimTextOutput("testeOUT"),
         h3("Results:"),
-        markdown("> **Obs**: The rows in the resulting table are selectable for download. 
-        By default (with no selection) all rows are used for download. 
-        Users can visualize the selected features in the JBrowse by downloading it as 
-        a GFF file and inserting it in the Browser by clicking in the button \"Track\", 
+        markdown("> **Obs**: The rows in the resulting table are selectable for download.
+        By default (with no selection) all rows are used for download.
+        Users can visualize the selected features in the JBrowse by downloading it as
+        a GFF file and inserting it in the Browser by clicking in the button \"Track\",
         in the Genome Browser."),
         actionButton("reset", "Reset row selection"),
         br(),
@@ -69,56 +69,56 @@ parse_sql <- function(sqldb) {
         downloadButton("downloadData", "Download")
       )
     ),
-    
+
     server = function(input, output, session) {
-      
+
       ##############################
       ### Open DBs in dataframes ###
       ##############################
       openTable <- reactive({
         as.data.frame(tbl(sqldb, sql(paste0("SELECT * FROM GFF"))))
       })
-      
+
       openTableNucl <- reactive({
         as.data.frame(tbl(sqldb, sql(paste0("SELECT * FROM NucleotideFasta"))))
       })
-      
+
       openTableProt <- reactive({
         as.data.frame(tbl(sqldb, sql(paste0("SELECT * FROM ProteinFasta"))))
       })
-      
+
       ##############################
       ### Create filter handlers ###
       ##############################
-      
+
       # Create selection for contigs
       output$contigs <- renderUI({
         selectInput("contigs", "Contigs:",
-                    unique(sort(openTable()[,1])), 
+                    unique(sort(openTable()[,1])),
                     multiple = TRUE)
       })
-      
+
       # Create selection for sources
       output$sources <- renderUI({
         selectInput("sources", "Sources:",
-                    sort(c("CARD", "AMRFinderPlus", "barrnap", "PHAST", 
-                           "ICEberg", "VFDB", "Victors", "Prodigal", 
-                           "Aragorn", "digIS")), 
+                    sort(c("CARD", "AMRFinderPlus", "barrnap", "PHAST",
+                           "ICEberg", "VFDB", "Victors", "Prodigal",
+                           "Aragorn", "digIS")),
                     multiple = TRUE)
       })
-      
+
       # Create selection for features
       output$features <- renderUI({
         selectInput("features", "Features:",
-                    sort(c("CDS", "tRNA", "rRNA", "ICE", 
-                           "Resistance", "Virulence", "Prophage", "transposable_element")), 
+                    sort(c("CDS", "tRNA", "rRNA", "ICE",
+                           "Resistance", "Virulence", "Prophage", "transposable_element")),
                     multiple = TRUE)
       })
-      
+
       #####################################################
       ### Create additional filters based on 9th column ###
       #####################################################
-      
+
       # Parse pattern file
       attributes_patterns <- reactive({
         if (!is.null(input$att_patterns)) {
@@ -128,11 +128,11 @@ parse_sql <- function(sqldb) {
           ""
         }
       })
-      
+
       #############################
       ### Parse applied filters ###
       #############################
-      
+
       ## Contig Filter
       contigFilter <- reactive({
         if (length(as.list(input$contigs)) > 0) {
@@ -141,25 +141,25 @@ parse_sql <- function(sqldb) {
           unique(sort(openTable()[,1]))
         }
       })
-      
+
       ## Source Filter
       sourceFilter <- reactive({
         if (length(as.list(input$sources)) > 0) {
           as.list(input$sources)
         } else {
-          sort(c("CARD", "AMRFinderPlus", "barrnap", "PHAST", "ICEberg", "VFDB", "Victors", "Prodigal", "Aragorn"))
+          sort(c("CARD", "AMRFinderPlus", "barrnap", "PHAST", "ICEberg", "VFDB", "Victors", "Prodigal", "Aragorn", "digIS"))
         }
       })
-      
+
       ## Feature Filter
       featureFilter <- reactive({
         if (length(as.list(input$features)) > 0) {
           as.list(input$features)
         } else {
-          sort(c("CDS", "tRNA", "rRNA", "ICE", "Resistance", "Virulence", "Prophage"))
+          sort(c("CDS", "tRNA", "rRNA", "ICE", "Resistance", "Virulence", "Prophage", "transposable_element"))
         }
       })
-      
+
       ## Start Filter
       startFilter <- reactive({
         if (isTruthy(input$start)) {
@@ -168,7 +168,7 @@ parse_sql <- function(sqldb) {
           as.integer(0)
         }
       })
-      
+
       ## End Filter
       endFilter <- reactive({
         if (isTruthy(input$end)) {
@@ -177,7 +177,7 @@ parse_sql <- function(sqldb) {
           Inf
         }
       })
-      
+
       ## Strand Filter
       strandFilter <- reactive({
         if (length(as.list(input$strand)) > 0) {
@@ -186,7 +186,7 @@ parse_sql <- function(sqldb) {
           sort(c("+", "-"))
         }
       })
-      
+
       # Filter data-frame
       filteredGFF <-
         reactive({
@@ -211,11 +211,11 @@ parse_sql <- function(sqldb) {
               arrange(ID)
           }
         })
-      
+
       #############################
       ### Render filtered table ###
       #############################
-      output$table <- 
+      output$table <-
         renderDataTable({
           datatable(filteredGFF() %>%
                       select(ID, Contig, Source, Feature, Start, End, Strand, Attributes),
@@ -233,10 +233,10 @@ parse_sql <- function(sqldb) {
                     )
           ) %>% formatStyle(
             columns = colnames(filteredGFF() %>%
-                                 select(ID, Contig, Source, Feature, Start, End, Strand, Attributes)), 
+                                 select(ID, Contig, Source, Feature, Start, End, Strand, Attributes)),
             fontSize = '10pt')
         })
-      
+
       ######################################
       ### Understand users row selection ###
       ######################################
@@ -247,9 +247,9 @@ parse_sql <- function(sqldb) {
           data
         } else {
           filteredGFF()
-        } 
+        }
       })
-      
+
       #########################################
       ### Reset row selection, if necessary ###
       #########################################
@@ -257,11 +257,11 @@ parse_sql <- function(sqldb) {
       observeEvent(input$reset, {
         DT::selectRows(myProxy, NULL)
       })
-      
+
       ##################################################################################
       ### Use selected genes to subset the NUCLEOTIDE and PROTEIN sequence databases ###
       ##################################################################################
-      
+
       # Get NUCL
       filteredNUCL <-
         reactive({
@@ -269,11 +269,11 @@ parse_sql <- function(sqldb) {
           nucl <- openTableNucl() %>%
             filter(ID %in% ids) %>%
             arrange(ID)
-          
+
           # Return FASTA
           glue('>{nucl$ID} {nucl$Description}\n{nucl$Sequence}')
         })
-      
+
       # Get PROT
       filteredPROT <-
         reactive({
@@ -281,16 +281,16 @@ parse_sql <- function(sqldb) {
           prot <- openTableProt() %>%
             filter(ID %in% ids) %>%
             arrange(ID)
-          
+
           # Return FASTA
           glue('>{prot$ID} {prot$Description}\n{prot$Sequence}')
         })
-      
+
       ##############################################################################
       ### Use selected genes to create the parsed attributes col for spreadsheet ###
       ##############################################################################
       attributes <- reactive({
-        
+
         # attribute fields
         fields <- c("ID", "gene", "product", "inference", "db_xref", "eC_number", "KO",
                     "NDARO:Gene_Name", "NDARO:Gene:Product", "NDARO:Closest_Sequence",
@@ -298,28 +298,28 @@ parse_sql <- function(sqldb) {
                     "CARD:Name", "CARD:Product", "CARD:Inference", "CARD:Targeted_drug_class",
                     "VFDB:Product", "VFDB:Target", "victors:Product", "victors:Target",
                     "ICEberg:Product", "ICEberg:Target", "phast:Product", "phast:Target", "note")
-        
+
         # Create empty data.frame for the parsed table
         parsed_attributes <- setNames(data.frame(
           matrix(ncol = 26, nrow = nrow(selectedData()))), fields
-          ) 
-        
+          )
+
         # Apply the getAttributes to each entry
         for(entry in fields) {
-          parsed_attributes[, as.character(entry)] <- 
+          parsed_attributes[, as.character(entry)] <-
             getAttributeField(as.character(selectedData()[,10]), as.character(entry), ";")
         }
-        
+
         # Output
         parsed_attributes
       })
-      
-      
+
+
       ########################################
       ### Final step, handle data download ###
       ########################################
       output$downloadData <- downloadHandler(
-        
+
         filename = function() {
           if(input$format == 'gff') {
             paste('data_download', '.gff', sep = '')
@@ -340,15 +340,15 @@ parse_sql <- function(sqldb) {
           } else if(input$format == 'fasta-prot') {
             write(filteredPROT(), file, sep = "\n")
           } else if(input$format == 'spreadsheet') {
-            write.xlsx(attributes(), file, sheetName="Parsed_Attributes", 
+            write.xlsx(attributes(), file, sheetName="Parsed_Attributes",
                        col.names=T, row.names=F, append=F)
             # write.xlsx(selectedData() %>%
-            #              select(-ID), file, sheetName="Original_GFF", 
+            #              select(-ID), file, sheetName="Original_GFF",
             #            col.names=T, row.names=F, append=T)
           }
         }
       )
-      
+
     }
   )
 }
